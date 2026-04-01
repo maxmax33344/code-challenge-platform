6a9208c7325ee10b58e1807e42bee18ebb434234
Max Wong <kms111671@gmail.com>
2026-04-01 14:30:53 +1000

    Remove debug line in production code

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,3 +1,4 @@
+//fixing SRV record DNS resolution
 require("node:dns/promises").setServers(["1.1.1.1", "8.8.8.8"]);
 // config/db.js
 const mongoose = require("mongoose");
@@ -10,8 +11,7 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URI);  // Remove deprecated options
     console.log("MongoDB connected successfully");
   } catch (error) {
-    //console.error("MongoDB connection error:", error.message);
-    console.error("MongoDB connection error:", error);
+    console.error("MongoDB connection error:", error.message);
     process.exit(1);
   }
 };
