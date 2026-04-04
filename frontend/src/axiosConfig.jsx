de0e3f1e0d6ebdc4038b3f48a29e5311fd94fdbb
Max Wong <kms111671@gmail.com>
2026-04-04 20:19:40 +1000

    modify axiosConfig.jsx

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   //baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://52.62.251.198:5001', // live
+  baseURL: 'http://13.211.104.126:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 axiosInstance.interceptors.request.use((config) => {
